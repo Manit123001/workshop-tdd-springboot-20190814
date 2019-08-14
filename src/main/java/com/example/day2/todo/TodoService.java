5260687274518aa89a155ca10ff00104191c86cb
Manit Cholpinyo <manit.cholpinyo@krungsri.com>
2019-08-14 15:55:05 +0700

    query task from db

--- src/main/java/com/example/day2/todo/TodoService.java
+++ src/main/java/com/example/day2/todo/TodoService.java
@@ -1,5 +1,6 @@
 package com.example.day2.todo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -8,10 +9,20 @@ import java.util.List;
 @Service
 class TodoService {
 
-    List<TaskResponse> inquiryAll() {
+    @Autowired
+    private
+    TaskRepository taskRepository;
+
+    public List<TaskResponse> inquiryAll() {
+
+        // Get data from repository
+        List<Task> tasks = (List<Task>) taskRepository.findAll();
+
+        // Map to response
         List<TaskResponse> data  = new ArrayList<>();
-        data.add(new TaskResponse(1, "Task 1"));
-        data.add(new TaskResponse(2, "Task 2"));
+        tasks.forEach(task -> {
+            data.add(new TaskResponse(task.getId(), task.getName()));
+        });
 
         return data;
     }
--- src/test/java/com/example/day2/todo/TodoControllerTest.java
+++ src/test/java/com/example/day2/todo/TodoControllerTest.java
@@ -11,10 +11,6 @@ import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
